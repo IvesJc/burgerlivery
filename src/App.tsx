a7d98626f08427c0ac73475d1b23afb2be143dc8
Ives Chiba <ivesjundi@gmail.com>
2024-03-16 23:16:45 -0300

    added pages sobremesas, entradas, bebidas but not finished yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Hamburgers from "./pages/Hamburgers/Hamburgers";
+import Entradas from "./pages/Entradas/Entradas";
+import Bebidas from "./pages/Bebidas/Bebidas";
+import Sobremesas from "./pages/Sobremesas/Sobremesas";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hamburgers" element={<Hamburgers />} />
+        <Route path="/entradas" element={<Entradas />} />
+        <Route path="/bebidas" element={<Bebidas />} />
+        <Route path="/sobremesas" element={<Sobremesas />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,29 @@
-import { Layout } from "../../components";
+import { Button, Layout } from "../../components";
+import {
+  HomeActionContentElement,
+  HomeBgImage,
+  HomeWrapperElement,
+} from "./Home.style";
+import HomeBg from "../../assets/bg-top.jpg";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/hamburgers");
+  };
+
   return (
     <Layout>
-      <h1>Home</h1>
+      <HomeWrapperElement>
+        <HomeActionContentElement>
+          <h1>Hamburgers para todos os gostos</h1>
+          <p>Nós fazemos deliciosos hamburgers e entregamos na sua porta</p>
+          <Button onClick={handleClick}>Peça agora</Button>
+        </HomeActionContentElement>
+        <HomeBgImage src={HomeBg} alt="placeholder" />
+      </HomeWrapperElement>
     </Layout>
   );
 }
